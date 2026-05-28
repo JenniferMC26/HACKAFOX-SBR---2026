5f15309a1b03d0fdf8227433f38495ed26313790
Jennifer Macedo <jn62902609@gmail.com>
2026-05-28 15:42:44 -0700

    feat: implementar app.dart y conectar tema y rutas

--- camino_front/lib/app.dart
+++ camino_front/lib/app.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:camino_front/shared/theme/app_theme.dart';
+import 'package:camino_front/shared/constants/app_routes.dart';
+import 'package:camino_front/shared/constants/app_strings.dart';
 
-// TODO: implementar App
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppStrings.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      routes: AppRoutes.routes,
+      initialRoute: AppRoutes.home,
+    );
+  }
+}
--- camino_front/lib/main.dart
+++ camino_front/lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
-import 'features/routing/screens/starting_Screen.dart';
-
+import 'package:camino_front/app.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const MapScreen(),
-    );
-  }
+  runApp(const App());
 }
