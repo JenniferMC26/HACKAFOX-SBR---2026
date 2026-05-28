cd0de31c27df223245ab340b310420302eaf51d4
Jennifer Macedo <jn62902609@gmail.com>
2026-05-28 15:32:52 -0700

    feat: implementar app_theme y app_text_styles

--- camino_front/lib/shared/theme/app_text_styles.dart
+++ camino_front/lib/shared/theme/app_text_styles.dart
@@ -1,3 +1,52 @@
 import 'package:flutter/material.dart';
 
-// TODO: implementar AppTextStyles
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const screenTitle = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w900,
+    letterSpacing: -0.5,
+  );
+
+  static const sectionTitle = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w800,
+  );
+
+  static const cardTitle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w800,
+    color: Colors.black,
+  );
+
+  static const body = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.black87,
+  );
+
+  static const bodyGrey = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.grey,
+  );
+
+  static const label = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black87,
+  );
+
+  static const labelGrey = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey,
+  );
+
+  static const caption = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    color: Colors.grey,
+  );
+}
--- camino_front/lib/shared/theme/app_theme.dart
+++ camino_front/lib/shared/theme/app_theme.dart
@@ -1,3 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:camino_front/shared/theme/app_colors.dart';
 
-// TODO: implementar AppTheme
+class AppTheme {
+  AppTheme._();
+  static ThemeData get light => ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      iconTheme: IconThemeData(color: Colors.black),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        foregroundColor: AppColors.primary,
+        side: BorderSide(color: AppColors.primary),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      ),
+    ),
+    chipTheme: ChipThemeData(side: BorderSide.none),
+  );
+}
