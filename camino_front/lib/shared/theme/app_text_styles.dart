730b1f3f59512ebb72e79f5409998df525d335a1
Jennifer Macedo <jn62902609@gmail.com>
2026-05-28 15:47:27 -0700

    feat: implementar app_theme y app_text_styles

--- camino_front/lib/shared/theme/app_text_styles.dart
+++ camino_front/lib/shared/theme/app_text_styles.dart
@@ -4,49 +4,26 @@ class AppTextStyles {
   AppTextStyles._();
 
   static const screenTitle = TextStyle(
-    fontSize: 28,
-    fontWeight: FontWeight.w900,
-    letterSpacing: -0.5,
-  );
+    fontSize: 28, fontWeight: FontWeight.w900, letterSpacing: -0.5);
 
   static const sectionTitle = TextStyle(
-    fontSize: 22,
-    fontWeight: FontWeight.w800,
-  );
+    fontSize: 22, fontWeight: FontWeight.w800);
 
   static const cardTitle = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w800,
-    color: Colors.black,
-  );
+    fontSize: 18, fontWeight: FontWeight.w800, color: Colors.black);
 
   static const body = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w500,
-    color: Colors.black87,
-  );
+    fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black87);
 
   static const bodyGrey = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w500,
-    color: Colors.grey,
-  );
+    fontSize: 16, fontWeight: FontWeight.w500, color: Colors.grey);
 
   static const label = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w600,
-    color: Colors.black87,
-  );
+    fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black87);
 
   static const labelGrey = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w400,
-    color: Colors.grey,
-  );
+    fontSize: 14, fontWeight: FontWeight.w400, color: Colors.grey);
 
   static const caption = TextStyle(
-    fontSize: 13,
-    fontWeight: FontWeight.w500,
-    color: Colors.grey,
-  );
+    fontSize: 13, fontWeight: FontWeight.w500, color: Colors.grey);
 }
--- camino_front/lib/shared/theme/app_theme.dart
+++ camino_front/lib/shared/theme/app_theme.dart
@@ -11,23 +11,17 @@ class AppTheme {
       backgroundColor: Colors.white,
       elevation: 0,
       scrolledUnderElevation: 0,
-      iconTheme: IconThemeData(color: Colors.black),
-    ),
+      iconTheme: IconThemeData(color: Colors.black)),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
         foregroundColor: Colors.white,
         elevation: 0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-      ),
-    ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)))),
     outlinedButtonTheme: OutlinedButtonThemeData(
       style: OutlinedButton.styleFrom(
         foregroundColor: AppColors.primary,
         side: BorderSide(color: AppColors.primary),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-      ),
-    ),
-    chipTheme: ChipThemeData(side: BorderSide.none),
-  );
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)))),
+    chipTheme: ChipThemeData(side: BorderSide.none));
 }
